ec3b20d584ff82fd3d2d8f0afa231dd7a476cf81
paperfox <ndg.paperfox@gmail.com>
2024-11-27 17:52:35 -0500

    config for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "https://paperfox.github.io/art/",
+  server: {
+    mimeTypes: {
+      "application/json": ["json"],
+    },
+  },
+});
