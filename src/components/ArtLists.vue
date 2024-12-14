8a64f033acda307d9bb6e162b66180e9102d8b37
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2024-12-14 00:33:46 -0500

    more limited mobile styling

--- src/components/ArtLists.vue
+++ src/components/ArtLists.vue
@@ -12,18 +12,13 @@ const openModal = inject('openModal');
 const modal = (imgData) => {
   openModal(imgData);
 };
-
-// come back to this, there's gotta be a way to improve it
-const windowWidth = window.innerWidth;
-const isSmallScreen = windowWidth < 445;
 </script>
 
 <template>
   <ul class="art-list" aria-live="polite">
     <li v-for="(image, index) of images" :key="condense(image.title)">
       <div :class="`artpiece--${index}`">
-        <img :src="`./art/preview/${image.link}`" :alt="image.desc" v-if="!isSmallScreen" />
-        <img :src="`./art/${image.link}`" :alt="image.desc" v-else />
+        <img :src="`./art/preview/${image.link}`" :alt="image.desc" />
         <button type="button" class="open-modal" :data-open="`modal${index}`" @click="modal(image)">
           <span>{{ image.title }}</span>
         </button>
