24b4a87e25c24e015dbb96fe57b9e3f75229f45e
paperfox <ndg.paperfox@gmail.com>
2025-04-29 19:00:24 -0400

    masonry grid progress

--- src/components/ArtLists.vue
+++ src/components/ArtLists.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject } from 'vue';
+import { ref, reactive, inject } from 'vue';
 
 defineProps(['images']);
 
@@ -12,127 +12,42 @@ const openModal = inject('openModal');
 const modal = (imgData) => {
   openModal(imgData);
 };
+
+// Refs to track all image elements
+const imageRefs = ref([]);
+
+// Reactive array to store the heights of images
+const imageHeights = reactive([]);
+
+// Function to update the height of an image after it loads
+function updateImageHeight(index) {
+  const img = imageRefs.value[index]; // Get the image element
+  // if (img) {
+  //   imageHeights[index] = img.naturalHeight; // Store the height in the array
+  // }
+
+  if (img) {
+    const containerWidth = img.parentElement.offsetWidth; // Get the container's width
+    const aspectRatio = img.naturalHeight / img.naturalWidth; // Calculate the aspect ratio
+    imageHeights[index] = containerWidth * aspectRatio; // Calculate the height based on the container's width
+  }
+}
 </script>
 
 <template>
-  <!-- <ul class="art-list" aria-live="polite">
-    <li v-for="(image, index) of images" :key="condense(image.title)" class="item" style="height: 140px">
-      <div :class="`artpiece--${index}`">
-        <button type="button" class="btn-modal" :data-open="`modal${condense(image.title)}`" @click="modal(image)">
-          <figure>
-            <img :src="`./art/${image.link}`" :alt="image.desc" />
-            <figcaption>{{ image.title }}</figcaption>
-          </figure>
-        </button>
-      </div>
-    </li>
-  </ul> -->
-
   <ul class="art-list" aria-live="polite">
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 320px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 400px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 280px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 300px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 320px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 280px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 180px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 150px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 400px"></li>
-    <li class="item" style="height: 140px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 170px"></li>
-    <li class="item" style="height: 120px"></li>
-    <li class="item" style="height: 220px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 160px"></li>
-    <li class="item" style="height: 200px"></li>
-    <li class="item" style="height: 190px"></li>
-    <li class="item" style="height: 120px"></li>
+    <li
+      v-for="(image, index) of images"
+      :key="condense(image.title)"
+      class="item"
+      :style="{ height: `${imageHeights[index] / 4}px` }"
+    >
+      <button type="button" class="btn-modal" :data-open="`modal${condense(image.title)}`" @click="modal(image)">
+        <figure>
+          <img :src="`./art/${image.link}`" :alt="image.desc" ref="imageRefs" />
+          <figcaption>{{ image.title }}</figcaption>
+        </figure>
+      </button>
+    </li>
   </ul>
 </template>
