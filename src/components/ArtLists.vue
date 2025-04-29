7dd42f3de61a21b1a067ecee32c37928a74634f1
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2025-04-29 00:23:22 -0400

    begin masonry grid work

--- src/components/ArtLists.vue
+++ src/components/ArtLists.vue
@@ -15,8 +15,8 @@ const modal = (imgData) => {
 </script>
 
 <template>
-  <ul class="art-list" aria-live="polite">
-    <li v-for="(image, index) of images" :key="condense(image.title)">
+  <!-- <ul class="art-list" aria-live="polite">
+    <li v-for="(image, index) of images" :key="condense(image.title)" class="item" style="height: 140px">
       <div :class="`artpiece--${index}`">
         <button type="button" class="btn-modal" :data-open="`modal${condense(image.title)}`" @click="modal(image)">
           <figure>
@@ -26,5 +26,113 @@ const modal = (imgData) => {
         </button>
       </div>
     </li>
+  </ul> -->
+
+  <ul class="art-list" aria-live="polite">
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 320px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 400px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 280px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 300px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 320px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 280px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 180px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 150px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 400px"></li>
+    <li class="item" style="height: 140px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 170px"></li>
+    <li class="item" style="height: 120px"></li>
+    <li class="item" style="height: 220px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 160px"></li>
+    <li class="item" style="height: 200px"></li>
+    <li class="item" style="height: 190px"></li>
+    <li class="item" style="height: 120px"></li>
   </ul>
 </template>
