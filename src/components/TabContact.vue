7fd28501c45d8527680321eefad291fe8fcafdcd
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2025-05-02 21:42:11 -0400

    contact update

--- src/components/TabContact.vue
+++ src/components/TabContact.vue
@@ -46,6 +46,16 @@ const formElements = ref([
   },
 ]);
 
+const isSubmitting = ref(false);
+const handleSubmit = () => {
+  isSubmitting.value = true;
+  // Simulate form submission
+  setTimeout(() => {
+    isSubmitting.value = false;
+    alert('Form submitted!');
+  }, 2000);
+};
+
 console.log('Contact form elements:', formElements);
 </script>
 
@@ -54,16 +64,13 @@ console.log('Contact form elements:', formElements);
     <h3>Contact</h3>
     <div>
       <div>
-        <p>
-          You can find me on the web at these places, or drop me an email at
-          <a href="mailto:art.paperfox+webcontact@gmail.com">art.paperfox@gmail.com</a> if you have specific questions.
-        </p>
+        <p>You can find me on the web at these places, or drop me an email if you have specific questions.</p>
         <SocialLinks :showSocialTitles="true" />
       </div>
       <div>
         <form @submit.prevent="handleSubmit">
           <FormInput v-for="element in formElements" :key="element.id" v-bind="element" v-model="element.modelValue" />
-          <button type="submit" class="btn-submit">Send</button>
+          <button type="submit" class="btn-submit">{{ isSubmitting ? 'Sending' : 'Send' }}</button>
         </form>
       </div>
     </div>
