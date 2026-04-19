e8c0777f32c6a5c5508f7f8f7f51183dcf489fe9
paperfox <ndg.paperfox@gmail.com>
2026-04-19 14:55:02 -0400

    begin newsletter work

--- src/components/TabEvents.vue
+++ src/components/TabEvents.vue
@@ -14,10 +14,29 @@
         <ul>
           <li>
             <p>
-              <strong>Natick Farmers' Market Winter Market</strong><br />
-              Saturday, March 7, 9am to 1pm - The Winter market is held at the Common Street Spiritual Center, right
-              next to the park where the outdoor market is usually held. I'll be downstairs, hope to see you there!<br />
-              13 Common St, Natick, MA 01760
+              <strong>Sweetwater Coffee & Tea Show</strong><br />
+              Now - May 31st - Natick Art Association group show is up and awesome at
+              <a href="https://www.sweetwaterscafe.com/ma-natick-center/" target="_blank" style="color: #a01818"
+                >Sweetwater</a
+              >. Stop by and see my and my association member friends' works while grabbing a tasty drink and a munchy.
+              <br />
+              19 S Main St, Natick, MA 01760
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Post Road Art Center Exhibition</strong><br />
+              April 20th - May 18th - My art will be up in the juried exhibition at
+              <a href="https://postroadartcenter.com/" target="_blank" style="color: #a01818">Post Road Art Center</a>,
+              "Spring Forward". Stop by to check it out and pick up art supplies!<br />
+              1 Boston Post Rd. E, Marlborough, MA 01752
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Art in Bloom</strong><br />
+              May 2 and 3 - My work 'Cycles' will be part of the Art in Bloom show at the Morse Institute Library. This
+              is a collaboration between Natick Art Association, the library, and the Garden club.
             </p>
           </li>
         </ul>
@@ -27,21 +46,16 @@
           If you stopped by at one of the many events I was at in 2025, it was great to meet you! I had so much fun over
           the past year, doing new things and meeting new folks. Hope to connect in 2026!
         </p>
-        <!-- <ul>
+        <ul>
           <li>
             <p>
-              <strong>Natick Open Studios</strong><br />
-              October 18 and 19, 2025 in downtown Natick, MA. I'll be at the First Congregational Church!
+              <strong>Natick Farmers' Market Winter Market</strong><br />
+              Saturday, March 7, 9am to 1pm - The Winter market is held at the Common Street Spiritual Center, right
+              next to the park where the outdoor market is usually held. I'll be downstairs, hope to see you there!<br />
+              13 Common St, Natick, MA 01760
             </p>
-            <a
-              href="https://www.natickartassociation.org/about-natick-open-studios"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              More info here
-            </a>
           </li>
-        </ul> -->
+        </ul>
       </div>
     </div>
   </div>
