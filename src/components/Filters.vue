018ad01953ae88cc4b56fe6203f0e2dfffe0dbb3
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2026-05-29 21:33:46 -0400

    remove count

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -89,7 +89,7 @@ const applyFilter = (filter, event) => {
           <span class="visually-hidden" v-if="activeFilterClass[filter.filterValue]"> active</span>
         </button> -->
       </div>
-      <p>Showing {{ filteredArts.length }} of {{ artwork.length }} art pieces</p>
+      <!-- <p>Showing {{ filteredArts.length }} of {{ artwork.length }} art pieces</p> -->
     </details>
     <ArtMasonry :images="filteredArts" />
   </div>
