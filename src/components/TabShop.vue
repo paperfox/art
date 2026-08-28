3311d98953242cdc5e477e41d8831bbfe392c3a6
paperfox <ndg.paperfox@gmail.com>
2026-08-27 23:43:32 -0400

    update shop tab

--- src/components/TabShop.vue
+++ src/components/TabShop.vue
@@ -1,46 +1,112 @@
-<script setup></script>
+<script setup>
+const items = [
+  // {
+  //   title: 'Originals',
+  // link: 'https://paperfoxmakesart.etsy.com?section_id=59103890',
+  //   image: '/shop-ad.jpg',
+  //   altText: 'Image of stickers, greeting cards, and prints on a desk.',
+  // },
+  {
+    title: 'Art Prints',
+    link: 'https://paperfoxmakesart.etsy.com?section_id=59103890',
+    image: '/images/art-prints.webp',
+    altText: 'Image of art prints of various watercolor paintings.',
+  },
+  {
+    title: 'Block Prints',
+    link: 'https://paperfoxmakesart.etsy.com?etsrc=sdt&section_id=59337743',
+    image: '/images/block-prints.webp',
+    altText:
+      'Image of a woodcut print of orange snapdragons, a woodcut print of a shiba inu, and a woodcut print of jackalopes.',
+  },
+  {
+    title: 'Stickers',
+    link: 'https://paperfoxmakesart.etsy.com?etsrc=sdt&section_id=55198926',
+    image: '/images/stickers.webp',
+    altText: 'Image of individual animal stickers and a canidae sticker pack.',
+  },
+  // {
+  //   title: 'Bookmarks',
+  //   link: 'https://paperfoxmakesart.etsy.com',
+  //   image: '/images/stickers.jpeg',
+  //   altText: 'Image of individual animal stickers and a canidae sticker pack.',
+  // },
+  // {
+  //   title: 'Greeting Cards',
+  // link: 'https://paperfoxmakesart.etsy.com?section_id=59103890',
+  //   image: '/images/stickers.jpeg',
+  //   altText: 'Image of individual animal stickers and a canidae sticker pack.',
+  // },
+];
+</script>
 
 <template>
   <div class="text-content">
     <h1>Shop</h1>
-    <p>For stickers, prints, and more, check out my shop!</p>
-    <div class="shop-banner">
-      <hr />
-      <a href="https://paperfoxmakesart.etsy.com" target="_blank" rel="noopener noreferrer" class="btn-outline"
-        >VISIT ETSY SHOP</a
-      >
-      <hr />
+    <p class="bottom-margin">
+      For stickers, prints, and more, check out
+      <a href="https://paperfoxmakesart.etsy.com" target="_blank">my Etsy shop!</a>
+    </p>
+    <div class="shop-container">
+      <a class="shop-item" v-for="item in items" :key="item.title" :href="item.link" target="_blank">
+        <div class="shop-image">
+          <img :src="item.image" :alt="item.altText" />
+        </div>
+        <div class="shop-text">
+          <h3>{{ item.title }}</h3>
+        </div>
+      </a>
     </div>
-    <img src="/shop-ad.jpg" alt="Image of stickers, greeting cards, and prints on a desk." />
   </div>
 </template>
 
 <style lang="css" scoped>
-.btn-outline {
-  width: fit-content;
-  display: inline-block;
-  white-space: nowrap;
-}
-
-.shop-banner {
+.shop-container {
   display: flex;
-  margin-bottom: var(--sm-spacing);
+  gap: var(--sm-spacing);
 
-  @media (max-width: 768px) {
-    hr {
-      display: none;
+  @media (max-width: 992px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .shop-item {
+    border: 1px solid var(--link);
+    min-width: 18rem;
+    text-decoration: none;
+
+    &:hover {
+      border-color: var(--text-body);
     }
-    .btn-outline {
+
+    .shop-image,
+    .shop-text {
       width: 100%;
-      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .shop-image {
+      height: 20rem;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+      }
+    }
+
+    .shop-text {
+      height: 100%;
+      background-color: var(--secondary-bg);
+      padding-bottom: var(--base-spacing);
     }
   }
 }
 
-@media (min-width: 1151px) {
-  #etsy-iframe {
-    width: 102%;
-    margin-inline: -1%;
-  }
+.bottom-margin {
+  margin-bottom: var(--sm-spacing);
 }
 </style>
